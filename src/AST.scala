f7add6a16abacb2d876507e6d7c5685b30a98f00
Ivan Motyashov <argson@gmail.com>
2012-10-26 13:55:29 -0700

    Modified the AST

--- src/AST.scala
+++ src/AST.scala
@@ -1,26 +1,45 @@
-object AST {
-	abstract class Identifier //TODO: Create identifier
+package JSAnalyzer
 
-	abstract class Expression //TODO: Create Expression
-	case class LeftHandSideExpression() extends Expression //TODO: create this
-	case class AssignmentExpression() extends Expression //TODO: create this
+import scala.util.parsing.input.Positional
+
+object AST {  
+    abstract class ASTNode extends Positional
+  
+	abstract class Expression extends ASTNode //TODO: Create Expression
+	case class LeftHandSideExpression extends Expression //TODO: create this
+	case class AssignmentExpression extends Expression //TODO: create this
+	case class FunctionExpression extends Expression 
+	case class BinaryExpression(op: String, t1: Expression, t2: Expression) extends Expression
+	case class UnaryExpression(op: String, t1: Expression) extends Expression
 
 	abstract class Statement extends SourceElement //TODO: create statement
-	case class FunctionDeclaration() extends SourceElement //TODO: create this
+	case class FunctionDeclaration extends SourceElement //TODO: create this
+	
+	abstract class Literal extends Expression
+	
+	case class ArrayLiteral(value: List[AssignmentExpression]) extends Literal
+	case class ObjectLiteral(value: List[(Expression, AssignmentExpression)]) extends Literal
+	case class This extends Literal
+	case class DecimalLiteral(value: String) extends Literal
+    case class HexIntegerLiteral(value: String) extends Literal
+    case class StringLiteral(value: String) extends Literal
+    case class BooleanLiteral(value: Boolean) extends Literal
+    case class NullLiteral extends Literal
 
 	abstract class Operation //TODO: create Operation
 
-
+	case class Identifier(value: String) extends Expression
+	
 	//From page 26 at ecma262
-	case class Block(sl : List[Statement]) extends Statement
+	case class Block(sl : Option[List[Statement]]) extends Statement
 	case class VariableStatement(vds: List[VariableDeclaration]) extends Statement
-	case class EmptyStatement() extends Statement
+	case class EmptyStatement extends Statement
 	case class ExpressionStatement(e:Expression) extends Statement
 	case class IfStatement(e:Expression,s1: Statement,s2:Option[Statement]) extends Statement
 	case class WhileStatement(e:Expression, s:Statement) extends Statement
-	case class DoWhileStatement() extends Statement 				//TODO: Create this, Should we maybe leave this out
+	case class DoWhileStatement extends Statement 				//TODO: Create this, Should we maybe leave this out
 	case class ForStatement(e1:Option[Expression],e2:Option[Expression],e3:Option[Expression],s:Statement) extends Statement
-	case class ForInStatement( ) extends Statement 					//TODO: Create this, Should we maybe leave this out
+	case class ForInStatement extends Statement 					//TODO: Create this, Should we maybe leave this out
 	case class ContinueStatement(i:Option[Identifier]) extends Statement
 	case class BreakStatement(i:Option[Identifier]) extends Statement
 	case class ReturnStatement(e: Option[Expression]) extends Statement 
@@ -29,8 +48,7 @@ object AST {
 	case class SwitchStatement(e:Expression,cb:CaseBlock) extends Statement
 	case class ThrowStatement(e:Expression) extends Statement
 	case class TryStatement(b:Block,c:Option[Catch], f:Option[Finally]) extends Statement
-	case class DebuggerStatement() extends Statement
-
+	case class DebuggerStatement extends Statement
 
 	//Helper classes for statements
 	case class VariableDeclaration(i:Identifier,a:Option[AssignmentExpression])
@@ -41,7 +59,7 @@ object AST {
 
 
 	//From page 101 at ecma262
-	abstract class SourceElement
+	abstract class SourceElement extends ASTNode
 
 	case class SourceElements(se:SourceElement,ses:SourceElement)
 
