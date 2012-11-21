38521f23b44a08888c5e98b6c0bf22cc32a93f30
Jens Fogh <minifogh@gmail.com>
2012-11-20 18:03:40 -0800

    Removed testcode from GlobalFlowFunction

--- src/GlobalFlowFunction.scala
+++ src/GlobalFlowFunction.scala
@@ -4,17 +4,5 @@ object GlobalFlowFunction{
 	abstract class GFFunction[t] {
 		def globalFlowFunction(node:CFG.ControlFlowNode, info_in : List[t]) : t;
 	}
-	
-	class TestFlowFunction extends GFFunction[Int]{
-	  def globalFlowFunction(node:CFG.ControlFlowNode, info_in : List[Int]) : Int = {
-	    node match {
-	      case node : CFG.If => 4
-	      case node : CFG.Empty => 4
-	      case node : CFG.Expression => 4
-	      case node : CFG.Merge => 4/info_in.size
-	      case _ => info_in(0)
-	    }
-	  }
-	}
 }
 
