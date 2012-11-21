9ccc7d0057e6cd8937e7a26ae4f25d93d1e5caa4
Jens Fogh <minifogh@gmail.com>
2012-11-20 18:00:02 -0800

    Worklist algorithm
    Takes GFF,Lattice and CFG as params
    Returns a map from edges, to info

---
+++ src/WorklistAlgorithm.scala
@@ -0,0 +1,43 @@
+package JSAnalyzer
+
+object WorklistAlgorithm1{
+	def worklistalgorithm[t](globalflowfunction : GlobalFlowFunction.GFFunction[t], lattice : Lattice.AbstractLattice[t], cfg : CFG.ControlFlowGraph) : Map[(CFG.ControlFlowNode, CFG.ControlFlowNode), t] = {
+	  var worklistStart : List[CFG.ControlFlowNode] = cfg.nodes;
+	  var mapStart = cfg.edges.foldLeft(Map[(CFG.ControlFlowNode, CFG.ControlFlowNode),t]())((map,edge) => map+(edge -> lattice.getBottom))
+	  	  
+	  def iterateWorklist(info : (List[CFG.ControlFlowNode], Map[(CFG.ControlFlowNode, CFG.ControlFlowNode), t])) : (List[CFG.ControlFlowNode], Map[(CFG.ControlFlowNode, CFG.ControlFlowNode), t]) = {
+		  var (list,map) = info
+		  list.size match {
+		  	case 0 => (list,map)
+		  	case _ => 
+				var node : CFG.ControlFlowNode = list.first;
+				var newWorklist : List[CFG.ControlFlowNode] = list.slice(1,list.size);
+				var incomingEdges : List[(CFG.ControlFlowNode, CFG.ControlFlowNode)] = cfg.edges.filter{case(from,to) => to==node};
+				var outgoingEdges : List[(CFG.ControlFlowNode, CFG.ControlFlowNode)] = cfg.edges.filter{case(from,to) => from==node};
+				var info_in : List[t] = incomingEdges.foldLeft(List[t]())((list,edge) => map.get(edge) match {
+				  case Some(e) => e :: list
+				  case None => list
+				})
+				var info_out = globalflowfunction.globalFlowFunction(node, info_in);
+				
+				iterateWorklist(
+				    outgoingEdges.foldLeft((newWorklist,map))((info,edge) => {
+						var (newlist,newmap) = info
+						if(lattice.getLub(newmap(edge),info_out) == newmap(edge)){
+							(newlist,newmap)
+						}else{
+							var newmap2 = newmap+(edge -> lattice.getLub(newmap(edge),info_out))
+							var (from,to) = edge
+							(to::list,newmap2)
+						}
+					}))
+	  }
+	}
+	var (_,newMap) = iterateWorklist((worklistStart,mapStart))	
+	newMap
+	}	
+}
+
+
+
+
