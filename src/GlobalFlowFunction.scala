a2bf1574e2cadc26ba7fa40585f1d71f70c17183
Jens Fogh <minifogh@gmail.com>
2012-11-24 12:03:46 -0800

    Lattice param added to signature of globalFlowFun
    Make it possible to use the lattice in the globalFlowFunction eg. in the
    Merge node

--- src/GlobalFlowFunction.scala
+++ src/GlobalFlowFunction.scala
@@ -2,7 +2,30 @@ package JSAnalyzer
 
 object GlobalFlowFunction{
 	abstract class GFFunction[t] {
-		def globalFlowFunction(node:CFG.ControlFlowNode, info_in : List[t]) : t;
+		def globalFlowFunction(node:CFG.ControlFlowNode, info_in : List[t], lattice : Lattice.AbstractLattice[t]) : t;
+	}
+	
+	class CSEFlowFunction extends GlobalFlowFunction.GFFunction[Map[AST.Identifier, List[AST.Expression]]] {
+	  def globalFlowFunction(node:CFG.ControlFlowNode, info_in : List[Map[AST.Identifier, List[AST.Expression]]], lattice : Lattice.AbstractLattice[Map[AST.Identifier, List[AST.Expression]]])= {
+		node match {
+	//		case CFG.Merge(_,_) => lattice.getGlb(info_in(0),info_in(1)) //TODO
+			case CFG.Continue(oe,_) => Map() //TODO
+			case CFG.Return(e,_) => Map() //TODO
+			case CFG.Expression(e,_) => Map() //TODO
+			case CFG.Assignment(i,oe,_) => oe match {
+											case Some (e) => e match {
+																case AST.AssignmentExpression (e1,op,e2) => Map() //TODO
+																case _ => println("TESTT");info_in.head+(i -> List(e))} //TODO
+											case None => info_in.head}
+			case CFG.If(e,_) => Map() //TODO
+			case CFG.DoWhile(e,_) => Map() //TODO
+			case CFG.ForIn(e1,e2,_) => Map() //TODO
+			case CFG.With(e,_) => Map() //TODO
+			case CFG.Switch(e,_) => Map() //TODO
+			case CFG.CaseClause(e,_) => Map() //TODO
+			case _ => Map()
+		}
+	  }
 	}
 }
 
--- src/WorklistAlgorithm.scala
+++ src/WorklistAlgorithm.scala
@@ -19,18 +19,23 @@ object WorklistAlgorithm{
 				  case Some(e) => e :: list
 				  case None => list
 				})
-				var info_out = globalflowfunction.globalFlowFunction(node, info_in);
+				println("TEST1")
+				var info_out = globalflowfunction.globalFlowFunction(node, info_in, lattice);
+				println("TEST2")
 				iterateWorklist(
 				    outgoingEdges.foldLeft((newWorklist,map))((info,edge) => {
+						println("TEST3")
 						var (newlist,newmap) = info
+						println("TEST4")
 						(lattice.compareElements(lattice.getLub(newmap(edge),info_out), newmap(edge)),lattice.compareElements(newmap(edge), lattice.getLub(newmap(edge),info_out))) match {
-							case (Some (true), Some (true)) => (newlist,newmap)
-							case (_,_) => var newmap2 = newmap+(edge -> lattice.getLub(newmap(edge),info_out))
+							case (Some (true), Some (true)) => println("TEST5");(newlist,newmap)
+							case (_,_) => println("TEST6"); var newmap2 = newmap+(edge -> lattice.getLub(newmap(edge),info_out))
 										  var (from,to) = edge
 										 (to::newlist,newmap2)
 					}}))
 	  }
 	}
+	println("TEST")
 	var (_,newMap) = iterateWorklist((worklistStart,mapStart))	
 	newMap
 	}	
