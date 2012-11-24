fbbd0c8c24a0a3b364b456b84a0cc347c34b53a4
Jens Fogh <minifogh@gmail.com>
2012-11-24 01:01:50 -0800

    Lattice compareElement changed to compareElements (S)!
    And added some parts of CSELattice

--- src/Lattice.scala
+++ src/Lattice.scala
@@ -7,12 +7,37 @@ package JSAnalyzer
 	  
 	  def getTop : t
 	   
-	  def compareElement(e1:t,e2:t) : Option[Boolean]
+	  def compareElements(e1:t,e2:t) : Option[Boolean]
 	  
 	  def getLub(e1:t, e2:t) : t
 	  
 	  def getGlb(e1:t, e2:t) : t
 	
 	}
+
+	class CSELattice(bottom : Map[AST.Identifier, List[AST.Expression]], top: Map[AST.Identifier, List[AST.Expression]]) extends Lattice.AbstractLattice[Map[AST.Identifier, List[AST.Expression]]](bottom, top){
+			  
+		  def getBottom : Map[AST.Identifier, List[AST.Expression]] = {
+			bottom;
+		  }
+		  
+		  def getTop : Map[AST.Identifier, List[AST.Expression]] = {
+			top;
+		  }
+		   
+		  def compareElements(m1:Map[AST.Identifier, List[AST.Expression]],m2:Map[AST.Identifier, List[AST.Expression]]) : Option[Boolean] = {
+			Some (true)
+		  }
+		  
+		  def getLub(m1:Map[AST.Identifier, List[AST.Expression]], m2:Map[AST.Identifier, List[AST.Expression]]) : Map[AST.Identifier, List[AST.Expression]] = {
+			m1.foldLeft(Map():Map[AST.Identifier, List[AST.Expression]]){case (map,(key,value)) => map+(key -> value.intersect(m2.apply(key)))}																	
+		  }
+		  
+		  def getGlb(m1:Map[AST.Identifier, List[AST.Expression]], m2:Map[AST.Identifier, List[AST.Expression]]) : Map[AST.Identifier, List[AST.Expression]] = {
+			m1.foldLeft(Map():Map[AST.Identifier, List[AST.Expression]]){case (map,(key,value)) => map+(key -> value.union(m2.apply(key)))}																	
+		  }
+		  
+		}
+
 	
 }
