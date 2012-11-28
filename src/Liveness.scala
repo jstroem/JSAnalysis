c25f57857f086b80d1ef74563f20cc870f703a2b
Jesper Lindstrøm Nielsen <me@jstroem.com>
2012-11-27 22:10:57 -0800

    Updated the liveness so it now compiles

--- src/Liveness.scala
+++ src/Liveness.scala
@@ -16,7 +16,7 @@ object Liveness {
 	  	else Some(false)
 	  }
 	  
-	  def getLub(m1:Map[AST.Identifier, List[AST.Expression]], m2:Map[AST.Identifier, List[AST.Expression]]) : List[AST.Identifier] = {
+	  def getLub(m1:List[AST.Identifier], m2:List[AST.Identifier]) : List[AST.Identifier] = {
 	  	m1.union(m2)
 	  }
 	  
@@ -27,7 +27,10 @@ object Liveness {
 	  def globalFlowFunction(node:CFG.ControlFlowNode, info_out : List[AST.Identifier], lattice : DataFlowAnalysis.DataFlowAnalysis[List[AST.Identifier]])= node match {
 	//		case CFG.Merge(_,_) => lattice.getGlb(info_in(0),info_in(1)) //TODO
 			case CFG.Continue(oi,_) => info_out
-			case CFG.Return(e,_) => expression(e,info_out)
+			case CFG.Return(e,_) => e match {
+				case Some(e) => expression(e,info_out)
+				case None => info_out
+			}
 			case CFG.Expression(e,_) => expression(e,info_out)
 			case CFG.Assignment(i,oe,_) => oe match {
 											case Some (e) => {
@@ -41,11 +44,14 @@ object Liveness {
 			}
 			case CFG.If(e,_) => expression(e,info_out)
 			case CFG.DoWhile(e,_) => expression(e,info_out)
-			case CFG.ForIn(e1,e2,_) => expression(e2,info_out) ::: expression(e1,info_out)
+			case CFG.ForIn(e1,e2,_) => e1 match { // e1 is an variable here and is only defined in the for statement so it should just be removed from the info.
+				case e1 : AST.Identifier => expression(e2,info_out.filter((node) => node != e1))
+				case _ => expression(e2,info_out) 
+			}
 			case CFG.With(e,_) => expression(e,info_out)
 			case CFG.Switch(e,_) => expression(e,info_out)
 			case CFG.CaseClause(e,_) => expression(e,info_out)
-			case DefaultClause(_) => info_out
+			case CFG.DefaultClause(_) => info_out
 			case CFG.Empty(_) => info_out
 			case CFG.Merge(_,_) => info_out
 		}
@@ -56,12 +62,12 @@ object Liveness {
 		case AST.AssignmentExpression(lhs, op, rhs) => lhs match {
 			case lhs: AST.Identifier => {
 				if (info_out.contains(lhs)) {
-					expression(e,info_out.filter((node) => node != lhs))
+					expression(rhs,info_out.filter((node) => node != lhs))
 				} else {
 					info_out
 				}
 			}
-			case _ => expression(rhs) //Cant figure anything out so every item used in the rhs is in use
+			case _ => expression(rhs,info_out) //Cant figure anything out so every item used in the rhs is in use
 		}
 		case AST.FunctionExpression(name, params, body) => info_out
 		case AST.BinaryExpression(op, t1, t2) => expression(t2,expression(t1,info_out))
@@ -70,15 +76,15 @@ object Liveness {
 		case AST.ConditionalExpression(cond, ifBranch, elseBranch) => expression(elseBranch,expression(ifBranch,expression(cond,info_out)))
 		case AST.CallExpression(callable, args) => args.getOrElse(List()).foldLeft(expression(callable,info_out))((list,exp) => expression(exp,list))
 		case AST.AllocationExpression(exp) => expression(exp,info_out)
-		case AST.MemberAccessExpression() => info_out
+		case AST.ArrayAccessExpression(e1,e2) => expression(e2,expression(e1,info_out))
 		case AST.ObjectAccessExpression(obj, member) => expression(obj,info_out)
 		case AST.Undefined() => info_out
-		case AST.Literal() => info_out
-		case AST.Identifier(v) => e :: info_out
+		case e : AST.Identifier => if (info_out.contains(e)) info_out else e :: info_out
+		case e : AST.Literal => info_out
 	}
 
 	def variables(cfg : CFG.ControlFlowGraph) : List[AST.Identifier] = {
-		cfg.nodes.foldLeft(List())((list,node) => node match {
+		cfg.nodes.foldLeft(List() : List[AST.Identifier])((list,node) => node match {
 			case CFG.Assignment(i,_,_) => i :: list
 			case _ => list
 		})
@@ -93,20 +99,20 @@ object Liveness {
 			def nodes() = {
 				var startList = if (startEndNodes) List(start,end) else List()
 				cfg.nodes.foldLeft(startList)((list,node) => {
-					GraphvizDrawer.Node(nodeId( node ), GraphvizDrawer.escape( nodeToString( node ) ) ) :: list
+					GraphvizDrawer.Node(CFGGrapher.nodeId( node ), GraphvizDrawer.escape( CFGGrapher.nodeToString( node ) ) ) :: list
 				})
 			}
 			def edges() = {
 				var startList = if (startEndNodes) {
-					var edge1 = GraphvizDrawer.Edge("start",nodeId(cfg.start))
-			 		var edge2 = GraphvizDrawer.Edge(nodeId(cfg.end),"end")
+					var edge1 = GraphvizDrawer.Edge("start",CFGGrapher.nodeId(cfg.start))
+			 		var edge2 = GraphvizDrawer.Edge(CFGGrapher.nodeId(cfg.end),"end")
 			 		List(edge1,edge2)
 				} else {
 					List()
 				}
 				cfg.edges.foldLeft(startList)((list,edge) => {
 					var (from,to) = edge
-					GraphvizDrawer.Edge(nodeId(from),nodeId(to), GraphvizDrawer.escape(AST.printList(liveness.getOrElse((from,to),List()),","))) :: list
+					GraphvizDrawer.Edge(CFGGrapher.nodeId(from),CFGGrapher.nodeId(to), Some(GraphvizDrawer.escape(AST.printList(liveness.getOrElse((from,to),List()),",")))) :: list
 				})
 			}
 
